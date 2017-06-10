bfc5847becdf12e156c679f07a6e5ccd507e7ce3
Brian Hulette <hulettbh@gmail.com>
2017-06-10 17:23:18 -0400

    initial commit

---
+++ udev.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+LOG=/var/strava-upload/upload.log
+
+if [ "${ACTION}" = add -a -e "${DEVNAME}" -a "${ID_FS_LABEL}" = GARMIN ]; then
+    DIR=`mktemp -d`
+    echo "Mounting to ${DIR}" >> ${LOG}
+    mount $DEVNAME $DIR
+    for f in ${DIR}/GARMIN/ACTIVITY/*.FIT; do
+        if [-f $f]; then
+            echo "Uploading $f..." >> ${LOG}
+            /var/strava-upload/upload.sh $f >> ${LOG}
+            if [$?]; then
+                echo "Success! Erasing $f" >> ${LOG}
+                rm $f
+            else
+                echo "Failed" >> ${LOG}
+            fi
+        fi
+    done
+    umount $DIR
+    rm -rf $DIR
+fi
---
+++ upload.sh
@@ -0,0 +1,41 @@
+#! /bin/bash
+
+FNAME=$1
+
+source config.sh
+dir="."
+token_file="${DIR}/.token"
+
+echo "looking for token in ${token_file}"
+if [ -f ${token_file} ]; then
+    access_token=`cat ${token_file}`
+else
+    echo "Authorize at the following URL:"
+    echo "https://www.strava.com/oauth/authorize?client_id=${STRAVA_API_CLIENTID}&response_type=code&scope=write&redirect_uri=http://theneuralbit.com"
+    echo ""
+
+    echo "code:"
+    read code
+    echo ""
+
+    access_token=`curl -X POST https://www.strava.com/oauth/token \
+        -F client_id=${STRAVA_API_CLIENTID} \
+        -F client_secret=${STRAVA_API_SECRET} \
+        -F code=${code} | jq -r .access_token`
+
+    echo -n "$access_token" > ${token_file}
+    echo "Got access token ${access_token}"
+fi
+
+RESULT=`curl -X POST https://www.strava.com/api/v3/uploads \
+    -H "Authorization: Bearer ${access_token}" \
+    -H activity_type=run \
+    -F file=@${FNAME} -F data_type=fit | jq -r .errors`
+
+if [ "${RESULT}" == null ]; then
+    exit 0
+else
+    echo "Error uploading:"
+    echo "${RESULT}"
+    exit 1
+fi
